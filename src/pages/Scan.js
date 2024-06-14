b561f7f994dacc8e2707cffba828e147c7d0a14a
kcbalbus <kc.balbus@gmail.com>
2024-06-15 01:54:37 +0200

    Scan view - now it is possible to add products code manually

--- src/pages/Scan.js
+++ src/pages/Scan.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { useNavigate } from 'react-router-dom';
 import {Html5QrcodeScanner} from "html5-qrcode";
 import {scanStore} from "../stores/ScanStore";
@@ -6,7 +6,7 @@ import {scanStore} from "../stores/ScanStore";
 const Scan = () => {
 
     const navigate = useNavigate();
-
+    const [inputBarcode, setInputBarcode] = useState("");
 
     useEffect(() => {
 
@@ -17,9 +17,8 @@ const Scan = () => {
         );
 
         function onScanSuccess(decodedText, decodedResult) {
-            scanStore.setProductCode(decodedText);
             console.log(`Code matched = ${decodedText}`, decodedResult);
-            navigate("/Details");
+            barcodeSubmition(decodedText);
         }
 
         function onScanFailure(error) {}
@@ -30,11 +29,49 @@ const Scan = () => {
             html5QrcodeScanner.clear();
         }
 
-    }, [navigate]);
+    }, []);
+
+    const validateBarcode = (barcode) => {
+        const barcodePattern = /^(?=.*\d)[\d\-]+$/;
+
+        if (!barcodePattern.test(barcode)) {
+            return false;
+        }
+
+        const barcode_lengths = [8, 12, 13, 14];
+        return barcode_lengths.includes(barcode.length);
+    };
+
+    const handleInputChange = (e) => {
+        setInputBarcode(e.target.value);
+    };
+
+    const handleInputSubmit = () => {
+        barcodeSubmition(inputBarcode);
+    };
+
+    const barcodeSubmition = (barcode) => {
+        if (validateBarcode(barcode)) {
+            scanStore.setProductCode(barcode);
+            navigate("/Details");
+        } else {
+            alert("Invalid barcode");
+        }
+    }
 
     return (
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", margin: "20px"}}>
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", margin: "20px"}}>
+            <h2>Scan the Product Barcode</h2>
             <div id="reader" style={{ width: "800px"}}></div>
+            <h2>Or Enter the Product Code Manually</h2>
+            <input
+                type="text"
+                value={inputBarcode}
+                onChange={handleInputChange}
+                placeholder="Enter barcode"
+            />
+            <button onClick={handleInputSubmit}>Submit</button>
+
         </div>
     )
 }
