44edb0aebfee207bc319204699811fa7c35f1403
Wiarr3 <wiora.biznes@gmail.com>
2024-06-20 19:19:25 +0200

    Moved logic of collections from Analyze to HistoryStore.

--- src/pages/Analyze.js
+++ src/pages/Analyze.js
@@ -4,7 +4,6 @@ import { historyStore } from '../stores/HistoryStore';
 import { Container, Row, Col, Table } from 'react-bootstrap';
 import { Bar, Doughnut } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement } from 'chart.js';
-import { toJS } from 'mobx';
 
 ChartJS.register(
     CategoryScale,
@@ -24,101 +23,20 @@ const Analyze = observer(() => {
         fetchData();
     }, []);
 
-    const calculateStatistics = () => {
-        const totalSpent = historyStore.history.reduce((total, purchase) => total + purchase.quantity, 0);
-        const mostFrequentProduct = historyStore.history
-            .reduce((acc, purchase) => {
-                const name = purchase.product.name;
-                acc[name] = acc[name] ? acc[name] + purchase.quantity : purchase.quantity;
-                return acc;
-            }, {});
-        const productNames = Object.keys(mostFrequentProduct);
-        const mostFrequentProductName = productNames.reduce((a, b) => mostFrequentProduct[a] > mostFrequentProduct[b] ? a : b, productNames[0]);
-        const mostFrequentBrand = historyStore.history
-            .reduce((acc, purchase) => {
-                const brand = purchase.product.brand;
-                acc[brand] = acc[brand] ? acc[brand] + purchase.quantity : purchase.quantity;
-                return acc;
-            }, {});
-        const brandNames = Object.keys(mostFrequentBrand);
-        const mostFrequentBrandName = brandNames.reduce((a, b) => mostFrequentBrand[a] > mostFrequentBrand[b] ? a : b, brandNames[0]);
-
-        const scoreMap = {
-            'A': 4,
-            'B': 3,
-            'C': 2,
-            'D': 1
-        };
-
-        const totalScore = historyStore.history
-            .reduce((total, purchase) => total + (scoreMap[purchase.product.score] || 0), 0);
-
-        const averageScoreNumeric = totalScore / historyStore.history.length;
-
-        const numericToLetterScore = (averageScore) => {
-            if (averageScore >= 3.5) {
-                return 'A';
-            } else if (averageScore >= 2.5) {
-                return 'B';
-            } else if (averageScore >= 1.5) {
-                return 'C';
-            } else {
-                return 'D';
-            }
-        };
-
-        const averageScore = numericToLetterScore(averageScoreNumeric);
-
-        const scoreCount = historyStore.history.reduce((acc, purchase) => {
-            const score = purchase.product.score;
-            acc[score] = acc[score] ? acc[score] + 1 : 1;
-            return acc;
-        }, {});
-
-        const sortedProducts = productNames
-            .map(name => ({ name, quantity: mostFrequentProduct[name] }))
-            .sort((a, b) => b.quantity - a.quantity)
-            .slice(0, 10);
-
-        return { totalSpent, mostFrequentProduct, mostFrequentProductName, mostFrequentBrandName, averageScore, scoreCount, sortedProducts };
-    };
-
-    const calculateTagStatistics = () => {
-        const tagCategories = {
-            'Organic': 0,
-            'Vegan': 0,
-            'Gluten-Free': 0,
-            'Non-GMO': 0,
-            'Low Sugar': 0
-        };
-
-        historyStore.history.forEach(purchase => {
-            const productTags = toJS(purchase.product.tags);
-            productTags.forEach(tag => {
-                if (tagCategories[tag.name] !== undefined) {
-                    tagCategories[tag.name]++;
-                }
-            });
-        });
-
-        const totalPurchases = historyStore.history.length;
-        const tagPercentages = {};
-        Object.keys(tagCategories).forEach(tag => {
-            tagPercentages[tag] = (tagCategories[tag] / totalPurchases) * 100;
-        });
-
-        return tagPercentages;
-    };
-
-    const stats = calculateStatistics();
-    const tagStats = calculateTagStatistics();
+    const totalSpent = historyStore.getTotalSpent();
+    const { mostFrequentProductName } = historyStore.getMostFrequentProduct();
+    const mostFrequentBrandName = historyStore.getMostFrequentBrand();
+    const averageScore = historyStore.getAverageScore();
+    const scoreCount = historyStore.getScoreCount();
+    const sortedProducts = historyStore.getSortedProducts();
+    const tagStats = historyStore.getTagStatistics();
 
     const scoreChartData = {
         labels: ['A', 'B', 'C', 'D'],
         datasets: [
             {
                 label: 'Score Frequency',
-                data: ['A', 'B', 'C', 'D'].map(score => stats.scoreCount[score] || 0),
+                data: ['A', 'B', 'C', 'D'].map(score => scoreCount[score] || 0),
                 backgroundColor: [
                     'rgba(75, 192, 192, 0.6)',
                     'rgba(54, 162, 235, 0.6)',
@@ -161,7 +79,7 @@ const Analyze = observer(() => {
                         </tr>
                         </thead>
                         <tbody>
-                        {stats.sortedProducts.map((product, index) => (
+                        {sortedProducts.map((product, index) => (
                             <tr key={index}>
                                 <td>{index + 1}</td>
                                 <td>{product.name}</td>
--- src/stores/HistoryStore.js
+++ src/stores/HistoryStore.js
@@ -1,4 +1,4 @@
-import { makeAutoObservable } from "mobx";
+import {makeAutoObservable, toJS} from "mobx";
 import ApiService from "../services/ApiService";
 
 class HistoryStore {
@@ -19,6 +19,107 @@ class HistoryStore {
 
     setHistory(products) {
         this.history = products;
+    }
+
+    getTotalSpent() {
+        return this.history.reduce((total, purchase) => total + purchase.quantity, 0);
+    }
+
+    getMostFrequentProduct() {
+        const mostFrequentProduct = this.history
+            .reduce((acc, purchase) => {
+                const name = purchase.product.name;
+                acc[name] = acc[name] ? acc[name] + purchase.quantity : purchase.quantity;
+                return acc;
+            }, {});
+        const productNames = Object.keys(mostFrequentProduct);
+        const mostFrequentProductName = productNames.reduce((a, b) => mostFrequentProduct[a] > mostFrequentProduct[b] ? a : b, productNames[0]);
+        return { mostFrequentProduct, mostFrequentProductName };
+    }
+
+    getMostFrequentBrand() {
+        const mostFrequentBrand = this.history
+            .reduce((acc, purchase) => {
+                const brand = purchase.product.brand;
+                acc[brand] = acc[brand] ? acc[brand] + purchase.quantity : purchase.quantity;
+                return acc;
+            }, {});
+        const brandNames = Object.keys(mostFrequentBrand);
+        const mostFrequentBrandName = brandNames.reduce((a, b) => mostFrequentBrand[a] > mostFrequentBrand[b] ? a : b, brandNames[0]);
+        return mostFrequentBrandName;
+    }
+
+    getAverageScore() {
+        const scoreMap = {
+            'A': 4,
+            'B': 3,
+            'C': 2,
+            'D': 1
+        };
+
+        const totalScore = this.history
+            .reduce((total, purchase) => total + (scoreMap[purchase.product.score] || 0), 0);
+
+        const averageScoreNumeric = totalScore / this.history.length;
+
+        const numericToLetterScore = (averageScore) => {
+            if (averageScore >= 3.5) {
+                return 'A';
+            } else if (averageScore >= 2.5) {
+                return 'B';
+            } else if (averageScore >= 1.5) {
+                return 'C';
+            } else {
+                return 'D';
+            }
+        };
+
+        return numericToLetterScore(averageScoreNumeric);
+    }
+
+    getScoreCount() {
+        return this.history.reduce((acc, purchase) => {
+            const score = purchase.product.score;
+            acc[score] = acc[score] ? acc[score] + 1 : 1;
+            return acc;
+        }, {});
+    }
+
+    getSortedProducts() {
+        const mostFrequentProduct = this.getMostFrequentProduct().mostFrequentProduct;
+        const productNames = Object.keys(mostFrequentProduct);
+
+        return productNames
+            .map(name => ({ name, quantity: mostFrequentProduct[name] }))
+            .sort((a, b) => b.quantity - a.quantity)
+            .slice(0, 10);
+    }
+
+    getTagStatistics() {
+        const tagCategories = {
+            'Organic': 0,
+            'Vegan': 0,
+            'Gluten-Free': 0,
+            'Non-GMO': 0,
+            'Low Sugar': 0
+        };
+
+        this.history.forEach(purchase => {
+            const productTags = toJS(purchase.product.tags);
+            productTags.forEach(tag => {
+                if (tagCategories[tag.name] !== undefined) {
+                    tagCategories[tag.name]++;
+                }
+            });
+        });
+
+        const totalPurchases = this.history.length;
+        const tagPercentages = {};
+        Object.keys(tagCategories).forEach(tag => {
+            tagPercentages[tag] = (tagCategories[tag] / totalPurchases) * 100;
+        });
+
+        return tagPercentages;
     }
 }
 
