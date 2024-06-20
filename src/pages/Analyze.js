d985838bf58a8f9a87d8a892c7b34f49a046b814
Wiarr3 <wiora.biznes@gmail.com>
2024-06-20 18:49:33 +0200

    Nice looking analyze view

--- src/pages/Analyze.js
+++ src/pages/Analyze.js
@@ -1,8 +1,20 @@
 import React, { useEffect } from 'react';
 import { observer } from 'mobx-react';
 import { historyStore } from '../stores/HistoryStore';
-import { Container, Table } from 'react-bootstrap';
-//import './Analyze.css';
+import { Container, Row, Col, Table } from 'react-bootstrap';
+import { Bar, Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement } from 'chart.js';
+import { toJS } from 'mobx';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement
+);
 
 const Analyze = observer(() => {
     useEffect(() => {
@@ -13,31 +25,23 @@ const Analyze = observer(() => {
     }, []);
 
     const calculateStatistics = () => {
-        console.log(historyStore.history)
         const totalSpent = historyStore.history.reduce((total, purchase) => total + purchase.quantity, 0);
         const mostFrequentProduct = historyStore.history
-            .map(purchase => purchase.product.name)
-            .reduce((acc, name) => {
-                acc[name] = acc[name] ? acc[name] + 1 : 1;
+            .reduce((acc, purchase) => {
+                const name = purchase.product.name;
+                acc[name] = acc[name] ? acc[name] + purchase.quantity : purchase.quantity;
                 return acc;
             }, {});
-        const productNames = Object.keys(mostFrequentProduct)
-        const mostFrequentProductName = productNames.reduce((a, b) => {
-            a = mostFrequentProduct[a] > mostFrequentProduct[b] ? a : b
-            return a
-        },productNames[0]);
+        const productNames = Object.keys(mostFrequentProduct);
+        const mostFrequentProductName = productNames.reduce((a, b) => mostFrequentProduct[a] > mostFrequentProduct[b] ? a : b, productNames[0]);
         const mostFrequentBrand = historyStore.history
-            .map(purchase => purchase.product.brand)
-            .reduce((acc, name) => {
-                acc[name] = acc[name] ? acc[name] + 1 : 1;
+            .reduce((acc, purchase) => {
+                const brand = purchase.product.brand;
+                acc[brand] = acc[brand] ? acc[brand] + purchase.quantity : purchase.quantity;
                 return acc;
             }, {});
-        const brandNames = Object.keys(mostFrequentBrand)
-        const mostFrequentBrandName = brandNames.reduce((a, b) => {
-            a = mostFrequentBrand[a] > mostFrequentBrand[b] ? a : b
-            return a
-        },brandNames[0]);
-        console.log(totalSpent);
+        const brandNames = Object.keys(mostFrequentBrand);
+        const mostFrequentBrandName = brandNames.reduce((a, b) => mostFrequentBrand[a] > mostFrequentBrand[b] ? a : b, brandNames[0]);
 
         const scoreMap = {
             'A': 4,
@@ -65,42 +69,119 @@ const Analyze = observer(() => {
 
         const averageScore = numericToLetterScore(averageScoreNumeric);
 
-        return { totalSpent, mostFrequentProductName, mostFrequentBrandName, averageScore };
+        const scoreCount = historyStore.history.reduce((acc, purchase) => {
+            const score = purchase.product.score;
+            acc[score] = acc[score] ? acc[score] + 1 : 1;
+            return acc;
+        }, {});
+
+        const sortedProducts = productNames
+            .map(name => ({ name, quantity: mostFrequentProduct[name] }))
+            .sort((a, b) => b.quantity - a.quantity)
+            .slice(0, 10);
+
+        return { totalSpent, mostFrequentProduct, mostFrequentProductName, mostFrequentBrandName, averageScore, scoreCount, sortedProducts };
     };
 
+    const calculateTagStatistics = () => {
+        const tagCategories = {
+            'Organic': 0,
+            'Vegan': 0,
+            'Gluten-Free': 0,
+            'Non-GMO': 0,
+            'Low Sugar': 0
+        };
 
+        historyStore.history.forEach(purchase => {
+            const productTags = toJS(purchase.product.tags);
+            productTags.forEach(tag => {
+                if (tagCategories[tag.name] !== undefined) {
+                    tagCategories[tag.name]++;
+                }
+            });
+        });
+
+        const totalPurchases = historyStore.history.length;
+        const tagPercentages = {};
+        Object.keys(tagCategories).forEach(tag => {
+            tagPercentages[tag] = (tagCategories[tag] / totalPurchases) * 100;
+        });
+
+        return tagPercentages;
+    };
 
     const stats = calculateStatistics();
+    const tagStats = calculateTagStatistics();
+
+    const scoreChartData = {
+        labels: ['A', 'B', 'C', 'D'],
+        datasets: [
+            {
+                label: 'Score Frequency',
+                data: ['A', 'B', 'C', 'D'].map(score => stats.scoreCount[score] || 0),
+                backgroundColor: [
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(255, 99, 132, 0.6)'
+                ],
+            },
+        ],
+    };
+
+    const tagChartData = {
+        labels: Object.keys(tagStats),
+        datasets: [
+            {
+                label: 'Tag Categories (%)',
+                data: Object.values(tagStats),
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(153, 102, 255, 0.6)'
+                ],
+            },
+        ],
+    };
 
     return (
         <Container className="my-5">
             <h1 className="text-center mb-5">Purchase Analysis</h1>
-            <h3>Product count: {stats.totalSpent}</h3>
-            <h3>Most Frequent Product: {stats.mostFrequentProductName}</h3>
-            <h3>Most Frequent Brand: {stats.mostFrequentBrandName}</h3>
-            <h3>Average score: {stats.averageScore}</h3>
-            <Table striped bordered hover className="mt-5">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Product Name</th>
-                    <th>Brand</th>
-                    <th>Score</th>
-                    <th>Purchase Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                {historyStore.history.map((purchase, index) => (
-                    <tr key={index}>
-                        <td>{index + 1}</td>
-                        <td>{purchase.product.name}</td>
-                        <td>{purchase.product.brand}</td>
-                        <td>{purchase.product.score}</td>
-                        <td>{new Date(purchase.purchaseDate).toLocaleDateString()}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </Table>
+            <Row className="gx-10">
+                <Col md={6}>
+                    <h3 className="text-center">Most frequently bought</h3>
+                    <Table striped bordered hover>
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Product Name</th>
+                            <th>Quantity</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {stats.sortedProducts.map((product, index) => (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{product.name}</td>
+                                <td>{product.quantity}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </Table>
+                </Col>
+                <Col md={6} className="d-flex flex-column justify-content-around">
+                    <div className="mb-4">
+                        <h3 className="text-center">Score Frequency</h3>
+                        <Doughnut data={scoreChartData} />
+                    </div>
+                    <div>
+                        <h3 className="text-center mt-5">Tag Categories (%)</h3>
+                        <Bar data={tagChartData} className="mt-3" />
+                    </div>
+                </Col>
+            </Row>
         </Container>
     );
 });
