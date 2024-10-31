2465ca49801aadcb43630457ae05faaad35e1e65
kcbalbus <kc.balbus@gmail.com>
2024-10-31 18:49:33 +0100

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
-import {useKeycloak} from "@react-keycloak/web";
+import { useKeycloak } from "@react-keycloak/web";
+import logo from "../assets/logo-horizontal.svg";
+import { NavDropdown } from "react-bootstrap";
 
 function MainNavBar() {
 
@@ -18,26 +18,34 @@ function MainNavBar() {
     }
   };
 
-
   if (!initialized || !keycloak.authenticated) {
     return null;
   }
 
   return (
-      <Navbar expand="lg" className="custom-navbar">
+      <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">EATcareFULLY</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Brand as={Link} to="/">
+            <img
+                src={logo}
+                width="150"
+                className="d-inline-block align-top"
+                alt="EATcareFULLY"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/Scan">Scan</Nav.Link>
-              <NavDropdown title="More" id="basic-nav-dropdown">
-                <NavDropdown.Item as={Link} to="/History">History</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item as={Link} to="/Analyze">Analyze</NavDropdown.Item>
+              <Nav.Link as={Link} to="/History">History</Nav.Link>
+              <Nav.Link as={Link} to="/Analyze">Analyze</Nav.Link>
+            </Nav>
+            <Nav className="ms-auto">
+              <NavDropdown title={`Logged in as: ${keycloak.tokenParsed?.preferred_username}`} id="basic-nav-dropdown">
+                <NavDropdown.Item onClick={handleLogout}>
+                  Logout
+                </NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
