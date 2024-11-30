f9289222f625d0294b2485f0630fcfaf8ca71dc7
Wiarr3 <wiora.biznes@gmail.com>
2024-11-30 02:02:57 +0100

    Remerged with the current version.

--- src/utils/URLS.js
+++ src/utils/URLS.js
@@ -2,6 +2,7 @@ export const API_ROOT_URL = 'http://localhost:8081/api';
 export const API_URLS = {
     products: `${API_ROOT_URL}/products`,
     purchases: `${API_ROOT_URL}/purchases`,
+    allpurchases: `${API_ROOT_URL}/purchases/all`,
     achievements: `${API_ROOT_URL}/achievements`,
     leaderboard: `${API_ROOT_URL}/leaderboard/me`,
     settings: `${API_ROOT_URL}/user-profile`,
