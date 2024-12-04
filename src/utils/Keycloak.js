c2836d27971dbae5f731abd3af1e6465fb580cc4
dawid splk <dawid.t.spalek@gmail.com>
2024-12-04 02:03:19 +0100

    Successful docker image build and run, connection to Keycloak to be fixed

--- src/utils/Keycloak.js
+++ src/utils/Keycloak.js
@@ -1,9 +1,9 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-    url: "http://localhost:8080/",
+    url: "http://keycloak:8080/",   // keycloak:8080 or localhost:8080?
     realm: "eat-carefully",
-    clientId: "frontend_client",
+    clientId: "frontend_client_docker", // check if keycloak redirect urls and other need fixing
 });
 
 export default keycloak;
