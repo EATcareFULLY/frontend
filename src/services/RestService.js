a9af68eec8dff46706283abf7733acfc462fc63c
kcbalbus <kc.balbus@gmail.com>
2024-12-06 00:26:05 +0100

    small refresh token update

--- src/services/RestService.js
+++ src/services/RestService.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import keycloak from "../utils/Keycloak";
+import {errorToast} from "../utils/Toasts";
+import {APP_URLS} from "../utils/URLS";
 
 class RestService {
     static async ajax(url, method, data, headers = {}, responseType = null, params = {}) {
         if (keycloak.authenticated) {
             await keycloak.updateToken(30).catch(() => {
+                errorToast("Failed to refresh token. You are being logged out.");
                 console.log("failed to update token");
-                keycloak.logout();
+                keycloak.logout({ redirectUri: `${APP_URLS.home}` });
             });
 
             headers['Authorization'] = `Bearer ${keycloak.token}`;
