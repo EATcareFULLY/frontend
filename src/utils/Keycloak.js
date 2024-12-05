d5bb677983f345d8151216a139d85bf776bd62eb
dawid splk <dawid.t.spalek@gmail.com>
2024-12-05 09:44:43 +0100

    Handle SPA not working, fix urls

--- src/utils/Keycloak.js
+++ src/utils/Keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-    url: "http://keycloak:8080/",   // keycloak:8080 or localhost:8080?
+    url: "http://localhost:8080/",   // keycloak:8080 or localhost:8080?
     realm: "eat-carefully",
     clientId: "frontend_client_docker", // check if keycloak redirect urls and other need fixing
 });
